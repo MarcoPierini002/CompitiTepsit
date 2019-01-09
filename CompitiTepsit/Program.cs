eb739b76548a61259bd1eb36391d3349e01f9b3f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:01:13 +0100

    Add project files.

---
+++ CompitiTepsit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._01._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double errore = 0.000000095;
+            
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
